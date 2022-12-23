867833a5831d872b0edba74b0563c484e6ccfd1c
sonjajos <sonjajosanov98@gmail.com>
2022-12-23 12:21:13 +0100

    Fix deploy errors

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,11 @@
 import type { GatsbyConfig } from "gatsby";
 
-const path = require("path");
-
-require("dotenv").config({
-  path: `.env`,
-});
-
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Maric Company d.o.o`,
-    siteUrl: `https://www.yourdomain.tld`
+    description: ``,
+    author: `Maric Company d.o.o`,
+    // siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -24,12 +20,15 @@ const config: GatsbyConfig = {
     "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
     "gatsby-plugin-netlify",
+    {
+      resolve: `gatsby-plugin-env-variables`,
+    },
     "gatsby-transformer-sharp",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: path.join(__dirname, "src", "images"),
+        "name": "images",
+        "path": "./src/images/"
       },
     },
   ]
