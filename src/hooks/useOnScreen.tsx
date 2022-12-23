18d497360358fbeea282541b8c544d40d4f62a93
sonjajos <sonjajosanov98@gmail.com>
2022-12-23 22:27:30 +0100

    Handle Intersection Observer error

--- src/hooks/useOnScreen.tsx
+++ src/hooks/useOnScreen.tsx
@@ -1,20 +1,23 @@
 import React from "react"
 
 export default function useOnScreen(ref: React.RefObject<HTMLDivElement>) {
+  if (typeof IntersectionObserver !== "undefined") {
+    const [isIntersecting, setIntersecting] = React.useState(false)
 
-  const [isIntersecting, setIntersecting] = React.useState(false)
+    const observer = new IntersectionObserver(
+      ([entry]) => setIntersecting(entry.isIntersecting)
+    )
 
-  const observer = new IntersectionObserver(
-    ([entry]) => setIntersecting(entry.isIntersecting)
-  )
+    React.useEffect(() => {
+      if (ref?.current) {
+        observer.observe(ref.current);
+        // Remove the observer as soon as the component is unmounted
+        return () => { observer.disconnect() }
+      }
+    }, []);
 
-  React.useEffect(() => {
-    if (ref?.current) {
-      observer.observe(ref.current);
-      // Remove the observer as soon as the component is unmounted
-      return () => { observer.disconnect() }
-    }
-  }, []);
+    return isIntersecting;
+  }
 
-  return isIntersecting
+  return false;
 };
