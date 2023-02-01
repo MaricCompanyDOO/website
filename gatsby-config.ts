cbdfb366d14fbb702f5072cb5082e4f531f5081d
sonjajos <sonjajosanov98@gmail.com>
2023-02-01 22:42:48 +0100

    Add sitemap

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,7 +11,7 @@ const config: GatsbyConfig = {
     title: `Maric Company d.o.o`,
     description: `Naša djelatnost je proizvodnja visokokvalitetnih drvnih sortimenta (rezane građe).`,
     author: `Maric Company d.o.o`,
-    siteUrl: `https://maric-company.netlify.app/`
+    siteUrl: `https://maric-company.netlify.app`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -20,7 +20,12 @@ const config: GatsbyConfig = {
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: '/sitemap'
+      }
+    },
     "gatsby-plugin-sharp",
     `gatsby-plugin-react-helmet`,
     "gatsby-plugin-netlify",
